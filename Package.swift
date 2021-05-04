4bad43d87be714d40c888834ba119cf4fa3871ad
Quintus Chen <quintus.guo.chen@outlook.com>
2021-05-05 01:33:12 +1200

    path fixed

--- Package.swift
+++ Package.swift
@@ -20,11 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VerifoneSDK",
-            sources: "VerifoneSDK/Sources/",
+            path: "VerifoneSDK/Sources",
             dependencies: []),
         .testTarget(
             name: "VerifoneSDKTests",
-            sources: "VerifoneSDK/Tests/",
+            path: "VerifoneSDK/Tests",
             dependencies: ["VerifoneSDK"]),
     ]
 )
