9e03aa1eeafa4fb956ed3fe7352ef6b7b304256e
Quintus Chen <quintus.guo.chen@outlook.com>
2021-05-05 01:26:47 +1200

    Try out

---
+++ VerifoneSDKTestApp/VerifoneSDKTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerifoneSDKTestAppApp.swift
+//  VerifoneSDKTestApp
+//
+//  Created by Guo Chen on 5/05/21.
+//
+
+import SwiftUI
+
+@main
+struct VerifoneSDKTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
